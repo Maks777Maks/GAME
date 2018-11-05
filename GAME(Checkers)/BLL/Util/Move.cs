668277e3d9a9860f2c5ee283feadb976acaf6932
Олексій Чеботкевич <forest793@ukr.net>
2018-11-05 22:21:55 +0200

    Add Class Move to BLL

---
+++ GAME(Checkers)/BLL/Util/Move.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Util
+{
+    public class Move
+    {
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public bool Queen { get; set; }
+
+        public bool Border { get; set; }
+
+        public bool doubleWay { get; set; }
+
+        public bool goldWay { get; set; }
+
+
+    }
+}
