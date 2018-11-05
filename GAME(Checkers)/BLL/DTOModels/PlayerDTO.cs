34aee33c8a2d0ca1df314310668db17872153ce1
Олексій Чеботкевич <forest793@ukr.net>
2018-11-05 20:39:26 +0200

    Add DTOModels

---
+++ GAME(Checkers)/BLL/DTOModels/PlayerDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOModels
+{
+    [DataContract]
+    public class PlayerDTO
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string NickName { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public int Victory { get; set; }
+        [DataMember]
+        public int Draw { get; set; }
+        [DataMember]
+        public int Losing { get; set; }
+    }
+}
