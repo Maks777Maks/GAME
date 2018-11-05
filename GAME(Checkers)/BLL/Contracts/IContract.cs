8a334915392ddbd0b4af244bd60a4e2b5e9170ad
Олексій Чеботкевич <forest793@ukr.net>
2018-11-05 20:48:49 +0200

    Add Contracts

---
+++ GAME(Checkers)/BLL/Contracts/IContract.cs
@@ -0,0 +1,29 @@
+﻿using BLL.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Contracts
+{
+    [ServiceContract]
+    public interface IContract
+    {
+        [OperationContract]
+        List<PlayerDTO> GetPlayers();
+
+        [OperationContract]
+        void AddPlayer(PlayerDTO player);
+
+        [OperationContract]
+        void DeletePlayer(PlayerDTO player);
+
+
+
+
+
+
+    }
+}
