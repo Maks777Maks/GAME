fd34cc4a5cbc8a942e898d17645fad1c335464c0
Олексій Чеботкевич <forest793@ukr.net>
2018-11-05 20:18:33 +0200

    Add interfaces,models,Repositories and Context

---
+++ GAME(Checkers)/DAL/Models/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Player
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string NickName { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public int Victory { get; set; }
+
+        public int Draw { get; set; }
+
+        public int Losing { get; set; }
+
+    }
+}
---
+++ GAME(Checkers)/DAL/Repositories/Repository.cs
@@ -0,0 +1,57 @@
+﻿using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class Repository<T> : IRepos<T> where T : class
+    {
+        
+
+        private readonly Context db = new Context();
+        private readonly DbSet<T> Set;
+
+        public Repository()
+        {
+            Set = db.Set<T>();
+        }
+
+        public void Create(T Entity)
+        {
+            Set.Add(Entity);
+            db.SaveChanges();
+        }
+
+
+        public void Delete(T Entity)
+        {
+            Set.Attach(Entity);
+            Set.Remove(Entity);
+            db.SaveChanges();
+
+        }
+
+
+        public IEnumerable<T> GetAll()
+        {
+            return Set.AsNoTracking<T>().AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return Set.Find(id);
+        }
+
+        public void Update(T Entity)
+        {
+            db.Entry<T>(Entity).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+
+    }
+}
